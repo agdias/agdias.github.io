37d94759161e24f4a1d0ca5b937c995981096911
agdias <agdias@gmail.com>
2018-06-15 21:39:45 -0300

    .

--- angelo/statemanager/js/main.js
+++ angelo/statemanager/js/main.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js', {scope:'/angelo/statemanager'})
+  navigator.serviceWorker.register('sw.js', {scope:'/angelo/statemanager/'})
   .then((reg)=> {
     console.log('Registration succeeded. Scope is ' + reg.scope);
   }).catch(function(error) {
