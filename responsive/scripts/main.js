b94817e34ca742e7f8492a45a1152165808163ec
Angelo Dias <agdias@gmail.com>
2018-07-10 13:22:51 -0300

    First submission
    Site structure
    responsive scripts

---
+++ responsive/scripts/main.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector("#menu");
+const main = document.querySelector("main");
+const drawer = document.querySelector("#drawer");
+
+menu.addEventListener("click", (e) => {
+    drawer.classList.toggle('open');
+    e.stopPropagation();
+});
+
+main.addEventListener("click", (e) => {
+  drawer.classList.remove('open');
+});
